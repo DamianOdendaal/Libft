aa1b4d36b6991afbb0dd7b6d01c7cce474836acf
Damian ODENDAAL <dodendaa@c5r9s4.wethinkcode.co.za>
2019-05-30 16:47:14 +0200

    added more functions

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -14,7 +14,7 @@
 
 int	ft_isalpha(int c)
 {
-	if ((c > 'a' && c < 'z') || (c < 'A' && c > 122))
+	if ((c > 'a' && c < 'z') || (c < 'A' && c > 'Z'))
 	{
 		return (c);
 	}
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -14,5 +14,10 @@
 
 int ft_isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	if ( c >= '0' &&  c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+
 }
