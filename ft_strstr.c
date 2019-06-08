b2dd04d9dd262866aed0476783e208290e687a4c
Tyrone Oggen <tyroneoggen@Tyrones-MacBook-Pro.local>
2019-06-08 19:48:02 +0200

    added strstr

---
+++ ft_strstr.c
@@ -0,0 +1,42 @@
+// include your header
+
+
+#include "libft.h"
+#include <string.h>
+
+
+char	compare(const char *s1, const char *s2);
+
+char	compare(const char *s1, const char *s2)
+{
+	int j;
+
+	j = 0;
+	while(s1[j] != s2[j])
+	{
+		j++;
+	}
+	return (0);
+}
+
+char	*ft_strstr(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		if (s1[i] == s2[i] && compare(s1,s2))
+		{
+			i++;
+		}
+		return ((char *)s2);
+		
+	}
+	 
+	if (s1[i] != s2[i])
+	{
+		return (NULL);
+	}
+	return ((char *)s2);
+}
