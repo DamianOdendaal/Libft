012d6da77cdb630382ebbef2752331f1031d76a6
Damian ODENDAAL <dodendaa@c5r9s7.wethinkcode.co.za>
2019-06-10 17:16:06 +0200

    memory

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,1 +1,14 @@
-char   *strnstr(const char *haystack, const char *needle, size_t len);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 12:15:50 by dodendaa          #+#    #+#             */
+/*   Updated: 2019/06/10 12:20:47 by dodendaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+char   *ft_strnstr(const char *haystack, const char *needle, size_t len);
