de0623175166779f392f3746c6747cb430bc24b7
Damian ODENDAAL <dodendaa@c5r9s11.wethinkcode.co.za>
2019-06-11 12:11:14 +0200

    fix

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 15:27:15 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/06/10 11:48:02 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/06/11 12:01:15 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,13 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int diff;
-	while ((unsigned char)*s1 == (unsigned char)*s2)
+	
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != '\0')
-		{
 			s1++;
 			s2++;
-		}	
 	}
-	if (*s1 != *s2)
-	{
-		diff = (unsigned char)*s1 - (unsigned char)*s2;
-		return (diff);
-	}
-	else
-	{
+	if (*s1 == '\0' && *s2 == '\0')
 		return (0);
-	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
-
