176b4781227ccd12140afb281ad07317b9ee6e23
Damian ODENDAAL <dodendaa@c5r9s7.wethinkcode.co.za>
2019-06-12 15:22:59 +0200

    end of days work done

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,9 +6,19 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:16:02 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/06/10 12:16:06 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/06/12 15:06:36 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void    *ft_memalloc(size_t size);
+
+void	*ft_memalloc(size_t size)
+{
+	
+		void *a;
+
+		if (!(a = malloc(size)))
+			return(NULL);
+			ft_bzero(a, size);
+		return (a);		
+}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,12 +6,27 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 10:06:10 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/06/12 11:04:13 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/06/12 15:02:05 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*strrchr(const char *s, int c)
 {
-	
+	 char	*str;
+
+	str = ( char *)s;
+	while (*str != c)
+	{
+		if (*str == '\0')
+		{
+			return (NULL);
+		}
+		if (*str == c )
+		{
+			return (str);
+		}
+		str++;
+	}
+	return (str);
 }
