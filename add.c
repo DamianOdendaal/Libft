2ecc86ef2f7a559ab62de709d1b3f3d1679ffaa5
Hezron Ngobeni <hngobeni@c4r5s5.wethinkcode.co.za>
2020-01-14 12:11:38 +0200

    done

---
+++ add.c
@@ -0,0 +1,23 @@
+gwo[rgmkwgowbg	p]oghe
+er
+gh
+rwehrwe
+hre
+hrh
+dh
+bfdahe
+net
+htej
+wryk
+ty
+ykr
+ky
+ytwlytw
+[psbPREMONMrei0hb]pwjTE
+MET
+MTEk
+rt
+ktqk
+t
+tq
+
---
+++ git.sh
@@ -0,0 +1,5 @@
+#!usr/bin/bash
+
+git add -A
+git commit -m "done"
+git push
